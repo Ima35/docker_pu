66f43f4aceab0d629a29d41798f9ee748fb535a5
Imairi <m.imairi.sys23@morijyobi.ac.jp>
2024-06-28 10:24:10 +0900

    todo_app

---
+++ todo_app/mytodo/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Task
+from .forms import TaskForm
+
+
+# Create your views here.
+class IndexView(View):
+    def get(self, request):
+        #todoリストを取得
+        todo_list = Task.objects.all()
+        
+        context = {
+            "todo_list": todo_list
+        }
+        
+        #テンプレートをレンダリング
+        return render(request, "mytodo/index.html", context)
+
+class AddView(View):
+    def get(self, request):
+        form = TaskForm()
+        #テンプレートのレンダリング処理
+        return render(request, "mytodo/add.html", {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        #登録処理
+        #入力データをフォームに渡す
+        form = TaskForm(request.POST)
+        #入力フォームに誤りがないかチェック
+        is_valid = form.is_valid()
+        
+        #でーたがせいじょうであれば
+        if is_valid:
+            #モデルに登録
+            form.save()
+            return redirect("/")
+        
+        #データが正常じゃない
+        return render(request, "mytodo/add.html", {'form': form})
+    
+
+class Update_task_complete(View):
+    def post(self, request, *args, **kwargs):
+        task_id = request.POST.get('task_id')
+        
+        task = Task.objects.get(id=task_id)
+        task.complete = not task.complete
+        task.save()
+        
+        return redirect('/')
+    
+class EditView(View):
+    def get(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        form = TaskForm(instance=task)
+        return render(request, "mytodo/edit.html",{'form': form, 'task': task})
+    
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+        return render(request, "mytodo/edit.html", {'form': form, 'task': task})
+
+class DeleteView(View):
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        task.delete()
+        return redirect("/")
+    
+    
+#ビュークラスをインスタンス化 
+index = IndexView.as_view()
+add = AddView.as_view()
+update_task_complete = Update_task_complete.as_view()
+edit = EditView.as_view()
+delete = DeleteView.as_view()
+
